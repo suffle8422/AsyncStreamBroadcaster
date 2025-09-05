e6d5672ee6bd8e97ea287ff665eb3bc7b14f9077
suffle8422 <inf.sky8422@gmail.com>
2025-09-05 09:51:04 +0900

    refactor: inner classをやめ、通常のclassにする

--- Sources/AsyncStreamBroadcaster/AsyncStreamBroadcaster.swift
+++ Sources/AsyncStreamBroadcaster/AsyncStreamBroadcaster.swift
@@ -5,79 +5,59 @@ import Foundation
 import os.lock
 
 /// ブロードキャストに対応したAsyncStreamを提供するクラス
-public struct AsyncStreamBroadcaster<Element: Sendable>: Sendable {
-    private let storage: Storage
-    
+public final class AsyncStreamBroadcaster<Element: Sendable>: @unchecked Sendable {
+    private let bufferingPolicy: AsyncStream<Element>.Continuation.BufferingPolicy
+    private let lock = OSAllocatedUnfairLock()
+    private var continuations: [UUID: AsyncStream<Element>.Continuation] = [:]
+
     public init(
         bufferingPolicy: AsyncStream<Element>.Continuation.BufferingPolicy = .unbounded
     ) {
-        self.storage = Storage(bufferingPolicy: bufferingPolicy)
+        self.bufferingPolicy = bufferingPolicy
     }
     
     /// 新しいStreamを作成
     public func makeStream() -> AsyncStream<Element> {
-        return storage.subscribe()
+        let id = UUID()
+        
+        let (stream, continuation) = AsyncStream<Element>.makeStream(
+            of: Element.self,
+            bufferingPolicy: bufferingPolicy
+        )
+        
+        lock.withLock {
+            continuations[id] = continuation
+        }
+        
+        continuation.onTermination = { [weak self] _ in
+            guard let self else { return }
+            _ = lock.withLock {
+                continuations.removeValue(forKey: id)
+            }
+        }
+        
+        return stream
     }
     
     /// 全てのStreamに値を送信
     public func yield(_ value: Element) {
-        storage.broadcast(value)
+        let currentContinuations = lock.withLock { continuations }
+        
+        for continuation in currentContinuations.values {
+            continuation.yield(value)
+        }
     }
     
     /// 全てのStreimに終了を通知
     public func finish() {
-        storage.finish()
-    }
-    
-    private final class Storage: @unchecked Sendable {
-        private var continuations: [UUID: AsyncStream<Element>.Continuation] = [:]
-        private let lock = OSAllocatedUnfairLock()
-        private let bufferingPolicy: AsyncStream<Element>.Continuation.BufferingPolicy
-        
-        init(bufferingPolicy: AsyncStream<Element>.Continuation.BufferingPolicy) {
-            self.bufferingPolicy = bufferingPolicy
+        let currentContinuations = lock.withLock {
+            let continuations = self.continuations
+            self.continuations.removeAll()
+            return continuations
         }
         
-        func subscribe() -> AsyncStream<Element> {
-            let id = UUID()
-            
-            let (stream, continuation) = AsyncStream<Element>.makeStream(
-                of: Element.self,
-                bufferingPolicy: bufferingPolicy
-            )
-            
-            lock.withLock {
-                continuations[id] = continuation
-            }
-            
-            continuation.onTermination = { [weak self] _ in
-                guard let self else { return }
-                _ = lock.withLock {
-                    continuations.removeValue(forKey: id)
-                }
-            }
-            
-            return stream
-        }
-        
-        func broadcast(_ value: Element) {
-            let currentContinuations = lock.withLock { continuations }
-            
-            for continuation in currentContinuations.values {
-                continuation.yield(value)
-            }
-        }
-        
-        func finish() {
-            let currentContinuations = lock.withLock {
-                let continuations = self.continuations
-                self.continuations.removeAll()
-                return continuations
-            }
-            
-            for continuation in currentContinuations.values {
-                continuation.finish()
-            }
+        for continuation in currentContinuations.values {
+            continuation.finish()
         }
     }
 }
