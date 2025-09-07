b80b69a2e59ad59e6ac47adc7d9283a99cd01089
suffle8422 <inf.sky8422@gmail.com>
2025-09-07 09:56:31 +0900

    feat: @unchecked SendableからSendableへ変更

--- Sources/AsyncStreamBroadcaster/AsyncStreamBroadcaster.swift
+++ Sources/AsyncStreamBroadcaster/AsyncStreamBroadcaster.swift
@@ -5,10 +5,9 @@ import Foundation
 import os.lock
 
 /// ブロードキャストに対応したAsyncStreamを提供するクラス
-public final class AsyncStreamBroadcaster<Element: Sendable>: @unchecked Sendable {
+public final class AsyncStreamBroadcaster<Element: Sendable>: Sendable {
     private let bufferingPolicy: AsyncStream<Element>.Continuation.BufferingPolicy
-    private let lock = OSAllocatedUnfairLock()
-    private var continuations: [UUID: AsyncStream<Element>.Continuation] = [:]
+    private let continuations = OSAllocatedUnfairLock<[UUID: AsyncStream<Element>.Continuation]>(initialState: [:])
 
     public init(
         bufferingPolicy: AsyncStream<Element>.Continuation.BufferingPolicy = .unbounded
@@ -25,14 +24,15 @@ public final class AsyncStreamBroadcaster<Element: Sendable>: @unchecked Sendabl
             bufferingPolicy: bufferingPolicy
         )
         
-        lock.withLock {
+        continuations.withLock { continuations in
             continuations[id] = continuation
         }
         
         continuation.onTermination = { [weak self] _ in
-            guard let self else { return }
-            _ = lock.withLock {
-                continuations.removeValue(forKey: id)
+            Task {
+                self?.continuations.withLock { continuations in
+                    continuations.removeValue(forKey: id)
+                }
             }
         }
         
@@ -41,23 +41,21 @@ public final class AsyncStreamBroadcaster<Element: Sendable>: @unchecked Sendabl
     
     /// 全てのStreamに値を送信
     public func yield(_ value: Element) {
-        let currentContinuations = lock.withLock { continuations }
-        
-        for continuation in currentContinuations.values {
-            continuation.yield(value)
+        continuations.withLock { continuations in
+            for continuation in continuations.values {
+                continuation.yield(value)
+            }
         }
     }
     
     /// 全てのStreimに終了を通知
     public func finish() {
-        let currentContinuations = lock.withLock {
-            let continuations = self.continuations
-            self.continuations.removeAll()
-            return continuations
-        }
-        
-        for continuation in currentContinuations.values {
-            continuation.finish()
+        continuations.withLock { continuations in
+            for continuation in continuations.values {
+                continuation.finish()
+            }
+            
+            continuations.removeAll()
         }
     }
 }
